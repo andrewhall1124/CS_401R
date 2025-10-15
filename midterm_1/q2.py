1dfbd07c0349b23743dd3b36fe5ac9f950569190
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-10-15 09:57:46 -0600

    Fixes to q2.

--- midterm_1/q2.py
+++ midterm_1/q2.py
@@ -29,24 +29,29 @@ pretty_print(E_Vk_xk)
 
 E_Vkp1_xkp1 = [0.0] * (N + 1)
 W = [10.33, 7.25, 17.15, 5.12, 33.21, 8.56, 43.32, 31.44, 22.90, 50, 0, 0]
+W_fv = [0.0] * (N + 1)
 policy = [''] * (N + 1)
 
 for k in range(N):
     E_Vkp1_xkp1[k] = E_Vk_xk[k + 1]
-    if E_Vkp1_xkp1[k] <= W[k]:
-        policy[k] = 'sell'
-    else:
-        policy[k] = 'hold'
+    W_fv[k] = W[k] * (1 + r) ** (N - k)
+    
+    if 'sell' not in policy:
+        if E_Vkp1_xkp1[k] <= W_fv[k]:
+                policy[k] = 'sell'
+        else:
+            policy[k] = 'hold'
 
-def pretty_print_table(E_Vkp1_xkp1, W, policy, fmt="{:.2f}"):
+def pretty_print_table(E_Vkp1_xkp1, W, W_fv, policy, fmt="{:.2f}"):
     """Pretty print E[V_{k+1}] and w_k values"""
-    header = f"\n{'k':<4} {'E[V_k+1(x_k+1)]':<16} {'w_k':<6} {'u_k':<4}"
+    header = f"\n{'k':<4} {'E[V_k+1(x_k+1)]':<16} {'w_k':<6} {'w_fv':<6} {'u_k':<4}"
     print(header)    
     for k in range(len(E_Vkp1_xkp1)):
         a = fmt.format(E_Vkp1_xkp1[k])
         b = fmt.format(W[k])
-        c = policy[k]
-        print(f"{k:<4} {a:<16} {b:<6} {c:<4}")
+        c = fmt.format(W_fv[k])
+        d = policy[k]
+        print(f"{k:<4} {a:<16} {b:<6} {c:<6} {d:<4}")
 
 
-pretty_print_table(E_Vkp1_xkp1, W, policy)
+pretty_print_table(E_Vkp1_xkp1, W, W_fv, policy)
