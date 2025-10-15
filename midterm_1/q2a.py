745583feafc8a8438c3c54ee3e38d38c51acea96
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-10-15 09:37:11 -0600

    Finished q2.

---
+++ midterm_1/q2a.py
@@ -0,0 +1,52 @@
+N = 10
+r = 0.1
+E_W = 100 / 3
+
+E_Vk_xk = [0.0] * (N + 1)
+
+def value_function(E_Vk_xk, k, N, r):
+    return (
+        (
+            (E_Vk_xk[k + 1] ** 3) / 
+            (7500 * (1 + r) ** (2 * (N - k)))
+        ) + 
+        (100 / 3) * (1 + r) ** (N- k)
+)
+
+E_Vk_xk[N] = 100 / 3
+
+for k in range(N)[::-1]:
+    E_Vk_xk[k] = value_function(E_Vk_xk, k, N, r)
+
+def pretty_print(E_Vk_xk):
+    header = "k    E_Vk_xk"
+
+    print(header)
+    for k, Vk_xk in enumerate(E_Vk_xk):
+        print(f"{k}    {round(Vk_xk, 2)}")
+
+pretty_print(E_Vk_xk)
+
+E_Vkp1_xkp1 = [0.0] * (N + 1)
+W = [10.33, 7.25, 17.15, 5.12, 33.21, 8.56, 43.32, 31.44, 22.90, 50, 0, 0]
+policy = [''] * (N + 1)
+
+for k in range(N):
+    E_Vkp1_xkp1[k] = E_Vk_xk[k + 1]
+    if E_Vkp1_xkp1[k] <= W[k]:
+        policy[k] = 'sell'
+    else:
+        policy[k] = 'hold'
+
+def pretty_print_table(E_Vkp1_xkp1, W, policy, fmt="{:.2f}"):
+    """Pretty print E[V_{k+1}] and w_k values"""
+    header = f"\n{'k':<4} {'E[V_k+1(x_k+1)]':<16} {'w_k':<6} {'u_k':<4}"
+    print(header)    
+    for k in range(len(E_Vkp1_xkp1)):
+        a = fmt.format(E_Vkp1_xkp1[k])
+        b = fmt.format(W[k])
+        c = policy[k]
+        print(f"{k:<4} {a:<16} {b:<6} {c:<4}")
+
+
+pretty_print_table(E_Vkp1_xkp1, W, policy)
